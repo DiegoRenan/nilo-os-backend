0ed467db9fb1fc5202e9ecc5a786446f35401f8f
DiegoRenan <renan.ccomp@gmail.com>
2019-09-30 10:46:50 -0300

    add concluded and updated to tickets serializer

--- app/serializers/v1/ticket_serializer.rb
+++ app/serializers/v1/ticket_serializer.rb
@@ -1,6 +1,6 @@
 module V1
   class TicketSerializer < ActiveModel::Serializer
-    attributes :id, :title, :body, :conclude_at, :nivel, :author, :responsibles, :created_at, :updated_at
+    attributes :id, :title, :body, :conclude_at, :nivel, :author, :responsibles, :created, :updated
 
     belongs_to :company
     belongs_to :department
@@ -8,6 +8,14 @@ module V1
     belongs_to :ticket_status
     belongs_to :ticket_type
     belongs_to :employee
+
+    def created
+      object.created_at
+    end
+    
+    def updated
+      object.updated_at
+    end
 
     def nivel
       object.priority.nivel
@@ -31,6 +39,8 @@ module V1
     def attributes(*args)
       h = super(*args)
       h[:conclude_at] = object.conclude_at.to_time.iso8601 unless object.conclude_at.blank?
+      h[:created_at] = object.created_at.to_time.iso8601 unless object.created_at.blank?
+      h[:updated_at] = object.updated_at.to_time.iso8601 unless object.updated_at.blank?
       h
     end
   end  
