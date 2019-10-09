7c23256a8adc8c886a480243e0146573ff61e00e
Renan Diego <renan.ccomp@gmail.com>
2019-10-09 15:20:42 -0300

    all attributes serializers inline

--- app/serializers/v1/ticket_serializer.rb
+++ app/serializers/v1/ticket_serializer.rb
@@ -1,6 +1,6 @@
 module V1
   class TicketSerializer < ActiveModel::Serializer
-    attributes :id, :title, :body  
+    attributes :id, :title, :body, :conclude, :nivel, :author, :created, :updated, :responsibles, :department_id, :sector_id, :ticket_status_id, :ticket_type_id, :priority_id  
 
     belongs_to :company
     belongs_to :department
@@ -31,7 +31,8 @@ module V1
 
     def responsibles
       if !object.nil?
-        object.responsibles.map { |resp| resp.employee.name}
+       responsibles = object.responsibles.map { |resp| resp.employee.name}
+       return responsibles
       else
         return []
       end
